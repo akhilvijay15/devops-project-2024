52b9efb77589a462bad5d336fd4ae0a218ea1569
akhil <akhil@gmail.com>
2024-04-12 15:32:42 +0530

    1st commit

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "dev-proj-1-remote-state-bucket-202404"
     key    = "devops-project-1/terraform.tfstate"
-    region = "ap-south-1"
+    region = "ap-southeast-1"
   }
 }
