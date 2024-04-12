18cc8851c4198e30e76285172892dd2a8b410410
akhil <akhil@gmail.com>
2024-04-12 16:32:24 +0530

    1st commit

--- infra/rds/main.tf
+++ infra/rds/main.tf
@@ -6,7 +6,7 @@ variable "mysql_username" {}
 variable "mysql_password" {}
 variable "mysql_dbname" {}
 
-# RDS Subnet Group
+
 resource "aws_db_subnet_group" "dev_proje_1_db_subnet_group" {
   name       = var.db_subnet_group_name
   subnet_ids = var.subnet_groups # replace with your private subnet IDs
